9397feb05545a8a6ee43a83f32b4d14350470b75
yaasien-s <Yaasiensalaam@gmail.com>
2022-02-10 08:59:41 +0200

    complete

--- src/views/Proj.vue
+++ src/views/Proj.vue
@@ -20,6 +20,9 @@
             d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
         </svg></router-link>
     </div>
+
+        <footer>© 2022 Yaasien Salaam ALL RIGHTS RESERVED</footer>
+
     
   </section>
 </template>
@@ -115,7 +118,7 @@
 
   .project h2:hover {
     transition: 00.3s;
-    
+
     background: #78b47c;
     color: #2b2b26;
     /* border: #2b2b26 1px solid; */
